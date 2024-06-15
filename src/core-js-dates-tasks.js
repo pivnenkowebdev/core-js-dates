aa1788582192d9c1c87a6d8a40b40650cba1d45b
Anton Pivnenko <pivnenkowebdev@gmail.com>
2024-06-15 07:09:10 +0400

    feat: getCountDaysOnPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -105,9 +105,15 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-02T00:00:00.000Z'  => 2
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const firstDate = new Date(dateStart);
+  const endDate = new Date(dateEnd);
+  const day = 86400000;
+  return Math.round((endDate - firstDate) / day + 1);
 }
+// 1. получить количество дней из первой даты
+// 2. получить количество дней из второй даты
+// 3. вычесть из большего меньшее и вернуть результат или Math.abs()
 
 /**
  * Returns true if a given date is within a specified range, including both the start and end dates.
